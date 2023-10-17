ab166deef5537d3696a74ac6314e877370d3d8dd
d0m <dginhac@u-bourgogne.fr>
2023-10-17 14:40:56 +0200

    ajout prenom a la classe Person

--- person.cpp
+++ person.cpp
@@ -1,9 +1,13 @@
 #include "person.h"
 
-Person::Person(std::string name, int month, int day) : _name(name), _birthday(month, day) {}
+Person::Person(std::string name, std::string firstname, int month, int day) : _name(name), _birthday(month, day) {}
 
 std::string Person::name() const {
     return _name;
+}
+
+std::string Person::firstname() const {
+    return _firstname;
 }
 
 Date Person::birthday() const {
--- person.h
+++ person.h
@@ -3,10 +3,12 @@
 
 class Person {
     public: 
-        Person(std::string name, int month, int day);
+        Person(std::string name, std::string firstname, int month, int day);
         std::string name() const;
+        std::string firstname() const;
         Date birthday() const;
     private:
         std::string _name;
+        std::string _firstname;
         Date _birthday;
 };
